19f4b14ed68afdc27a699ad12c4a6a144611e569
aleksagasparovski <aleksagasparovski@gmail.com>
2024-07-18 13:11:09 +0200

    calculations

--- calculations.py
+++ calculations.py
@@ -1,8 +1,9 @@
 a=int(input("unesite 1. broj"))
 b=int(input("unesite 2. broj"))
 proizvod=(a*b)
+zbir=(a+b)
 if(proizvod<=1000):
     print(proizvod)
 else:
-    print("Ovo se ne moze izracunati")
+    print(zbir)
 
